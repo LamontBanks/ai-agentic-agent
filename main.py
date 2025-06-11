cb80c15b64946c2f2777a028b4a4533bfe543b6b
Lamont Banks <lamontbanks@gmail.com>
2025-06-11 15:03:38 -0400

    Accept the prompt from the CLI

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import os
+import sys
+
 from dotenv import load_dotenv
 from google import genai
+
+# Usage: main.py "text prompt for google gemini"
+if len(sys.argv) == 1:
+    print("Usage: A double-quoted prompt is required, ex: \"List 3 beef summer cookout dishes\"")
+    sys.exit(1)
+user_prompt = sys.argv[1]
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -9,7 +17,7 @@ client = genai.Client(api_key=api_key)
 
 # Free-tier: model='gemini-2.0-flash-001'
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model='gemini-2.0-flash-001', contents=user_prompt
 )
 
 print(response.text)
