336334637d7b7c13de0cc9ae1d4b49551f66b7c7
Lamont Banks <lamontbanks@gmail.com>
2025-06-12 09:06:21 -0400

    Refactor for testing

---
+++ aiagent.py
@@ -0,0 +1,50 @@
+
+import os
+import sys
+
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+class AiAgent:
+    # Usage: main.py "text prompt for google gemini" [--verbose]
+    def __init__(self, args):
+        self.verbose_flag = False
+        self.user_prompt = ""
+
+        self._parseArgs(args)
+        
+        load_dotenv()
+        self.api_key = os.environ.get("GEMINI_API_KEY")
+
+    def send_prompt(self):
+        # Store the list of messages in the conversation
+        user_messages = [
+            types.Content(role="user", parts=[types.Part(text=self.user_prompt)])
+        ]
+
+        # Make requests
+        client = genai.Client(api_key=self.api_key)
+        response = client.models.generate_content(
+            # Free-tier: gemini-2.0-flash-001
+            model='gemini-2.0-flash-001', contents=user_messages
+        )
+
+        # Output
+        if self.verbose_flag:
+            print(f"User prompt: {self.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(response.text)
+
+    def _parseArgs(self, args):
+        if len(args) == 1:
+            print("Usage: A double-quoted prompt is required, ex: \"List 3 beef summer cookout dishes\"")
+            sys.exit(1)
+
+        if len(args) > 1:
+            self.user_prompt = args[1]
+
+        if len(args) == 3:
+            if args[2] == "--verbose" or args[2] == "-v":
+                self.verbose_flag = True
--- main.py
+++ main.py
@@ -1,42 +1,7 @@
-import os
+
 import sys
 
-from dotenv import load_dotenv
-from google import genai
-from google.genai import types
+from aiagent import AiAgent
 
-# Usage: main.py "text prompt for google gemini" [--verbose]
-if len(sys.argv) == 1:
-    print("Usage: A double-quoted prompt is required, ex: \"List 3 beef summer cookout dishes\"")
-    sys.exit(1)
-
-if len(sys.argv) > 1:
-    user_prompt = sys.argv[1]
-
-verbose_flag = False
-if len(sys.argv) == 3:
-    if sys.argv[2] == "--verbose" or sys.argv[2] == "-v":
-        verbose_flag = True
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-
-# Stores the list of messages in the conversation
-user_messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)])
-]
-
-# Make requests
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    # Free-tier: gemini-2.0-flash-001
-    model='gemini-2.0-flash-001', contents=user_messages
-)
-
-if verbose_flag:
-    print(f"User prompt: {user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-print(response.text)
+agent = AiAgent(sys.argv)
+agent.send_prompt()
